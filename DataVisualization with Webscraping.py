1167822d0512c6ee8a61959db4685a773da85e39
tsudhakarreddy <46421710+tsudhakarreddy@users.noreply.github.com>
2020-12-29 21:09:12 +0800

    Update DataVisualization with Webscraping.py

--- DataVisualization with Webscraping.py
+++ DataVisualization with Webscraping.py
@@ -11,7 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
-get_ipython().run_line_magic('matplotlib', 'inline')
+%matplotlib inline
 
 style.use('fivethirtyeight')
 fig = plt.figure()
