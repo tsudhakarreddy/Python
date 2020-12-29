0e5826d96a377b26246dff0789d57eb896f39000
tsudhakarreddy <46421710+tsudhakarreddy@users.noreply.github.com>
2020-12-29 21:32:54 +0800

    Update API_Stocks.py

--- API_Stocks.py
+++ API_Stocks.py
@@ -3,21 +3,17 @@
 
 # In[49]:
 
-
 #Real Time Stock Data Analysis with Python
 #1. Get the Stock Analysis API.
-#	>> pip3 install alpha-vantage
-#	
+#	>> pip3 install alpha-vantage	
 #2. Get the alpha-vantage API Key. (https://www.alphavantage.co/)
-#	>> Welcome to Alpha Vantage! Your API key is: MHP068BZNTQ23MKI. Please record this API key at a safe place for future data access.
-#	
+#	>> Welcome to Alpha Vantage! Your API key is: MHP068BZNTQ23MKI. Please record this API key at a safe place for future data access.	
 #3.	Write the Python code in Jupyter or pycharm.
 
 import pandas as pd
 from alpha_vantage.timeseries import TimeSeries
 import datetime
 import time
-
 
 API_KEY = 'MHP068BZNTQ23MKI'
 ts = TimeSeries(key=API_KEY, output_format='pandas')
@@ -33,18 +29,8 @@ def getPrice():
 
 getPrice()
 
-        
-
-
 ## look for closing value if the value is > 0.0004
 ##closing_value = data['4. close']
 ##Last_change = closing_value.pct_change()
 ##if abs(Last_change[-1])> 0.0003:
 ##    print('MS Alert : ' , Last_change[-1] )
-
-
-# In[ ]:
-
-
-
-
